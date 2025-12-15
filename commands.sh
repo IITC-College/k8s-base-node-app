24fe4e57b2126a44eca7855fd3d6ce8c4c3f699d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-12-15 08:47:19 +0200

    asd

--- commands.sh
+++ commands.sh
@@ -7,3 +7,15 @@ $ docker build -t lironefitoussi/kub-first-app:latest .
 $ docker push lironefitoussi/kub-first-app:latest
 
 $ kubectl create deployment first-app --image=lironefitoussi/kub-first-app:latest
+
+$ kubectl get deployments
+
+$ kubectl get pods
+
+$ kubectl get services
+
+$ kubectl get nodes
+
+$ kubectl get all
+
+$ kubectl get all --all-namespaces
